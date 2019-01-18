77e0e1588ed71b4175928766b3b2482db7b48352
caicai <shmily.cai@gmail.com>
2019-01-18 13:31:26 +0800

    react

--- src/section/nav/nav.js
+++ src/section/nav/nav.js
@@ -3,31 +3,35 @@ import { Menu, Icon } from 'antd';
 import { Link } from 'react-router-dom';
 import './nav.scss';
 
+const SubMenu = Menu.SubMenu;
+
+
 class Nav extends Component {
     render() {
         return (
             <div className="nav">
-                <div className="logo" />
+                <div className="logo" ></div>
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
                     <Menu.Item key="1">
                         <Icon type="home" />
                         <span>
-                            home
-                            {/* <Link to='/home'>Home</Link> */}
+                            <Link to='/home'>首 页</Link>
                         </span>
                     </Menu.Item>
-                    <Menu.Item key="2">
-                        <Icon type="user" />
-                        <span>
-                            <Link to='/user'>user</Link>
-                        </span>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                        <Icon type="upload" />
-                        <span>
-                            <Link to='/role'>role</Link>
-                        </span>
-                    </Menu.Item>
+                    <SubMenu key="sub1" title={<span><Icon type="global" /><span>系统管理</span></span>}>
+                        <Menu.Item key="5">
+                            <Link to='/user'>用户管理</Link>
+                        </Menu.Item>
+                        <Menu.Item key="6">
+                            <Link to='/role'>角色管理</Link>
+                        </Menu.Item>
+                        <Menu.Item key="7">
+                            权限管理
+                        </Menu.Item>
+                        <Menu.Item key="8">
+                            菜单管理
+                        </Menu.Item>
+                    </SubMenu>
                 </Menu>
             </div>
         );
